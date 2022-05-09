83a5ad593203c4fd48e0f17009611a97bf9eef5f
tajima <tejapan2003@gmail.com>
2022-05-09 15:02:29 +0900

    サイドバー表示時にサイドバー以外を非表示に変更

--- js/main.js
+++ js/main.js
@@ -2,5 +2,10 @@ const target = document.getElementById("hamburger-menu-button");
 target.addEventListener('click', () => {
   target.classList.toggle('open');
   document.getElementById("side-bar").classList.toggle('open');
+
+  let hiddenList = document.getElementsByClassName("hidden");
+  for (let i = 0; i < hiddenList.length; i++){
+    hiddenList[i].classList.toggle('open');
+  }
 });
 
